5edc4cd85fd791dc03caba522520970a6e6373e9
vladimirfp <vaf@itzapad.ru>
2022-03-10 21:40:07 +0000

    change08

--- disable_apple_upd.sh
+++ disable_apple_upd.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-curl -L -O https://github.com/valdimirfp/AppleUpdDisable/blob/main/AppleUpdDisable.taz
-tar -xvf AppleUpdDisable.taz
-cd AppleUpdDisable
-cat hosts >> /etc/hosts
-cat firewall >> /etc/pf.conf
+
+#download files from vladimirfp github
+mkdir AppleUpdDisable && cd AppleUpdDisable
+curl -o ./firewall.txt https://raw.githubusercontent.com/valdimirfp/AppleUpdDisable/main/firewall.txt
+curl -o ./hosts.txt https://raw.githubusercontent.com/valdimirfp/AppleUpdDisable/main/hosts.txt
+
+# add data to files
+cat hosts.txt >> /etc/hosts
+cat firewall.txt >> /etc/pf.conf
+
+# confurm settings
 pfctl -е -f /etc/pf.conf
 echo "disable apple updates complete" 
 
